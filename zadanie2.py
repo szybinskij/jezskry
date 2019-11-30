8774002f0562a80a6950e23151b306ea95a4d751
Kuba <szybinskij@gmail.com>
2019-11-30 06:44:18 +0100

    zadanie2

--- zadanie2.py
+++ zadanie2.py
@@ -12,7 +12,6 @@ print(list_to_sort)
 list_to_sort.sort(key=sortSecond)
 print(list_to_sort)
 
-
 print('wer2 lambda')
 list_to_sort = [[3, 2, 3], [2, 0, 2], [3, 0, 1], [3, 0, 3]]
 print(list_to_sort)
